fd22cba16080a89cbcaaccec56629117e6bd4f58
asconem <asconem@gmail.com>
2020-02-17 12:00:27 -0500

    WIPMultiplesDeleter

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -60,7 +60,23 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        Integer j = 0;
+        Integer[] buffer = new Integer[ints.length];
+
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] % 3 == 0) {
+                continue;
+            } else {
+                buffer[j] = ints[i];
+                j++;
+            }
+        }
+
+        Integer[] output = new Integer[j];
+        for (int k = 0; k < j; k++) {
+            output[k] = buffer[k];
+        }
+        return output;
     }
 
     /**
@@ -70,6 +86,22 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+        Integer j = 0;
+        Integer[] buffer = new Integer[ints.length];
+
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] % multiple == 0) {
+                continue;
+            } else {
+                buffer[j] = ints[i];
+                j++;
+            }
+        }
+
+        Integer[] output = new Integer[j];
+        for (int k = 0; k < j; k++) {
+            output[k] = buffer[k];
+        }
+        return output;
     }
 }
