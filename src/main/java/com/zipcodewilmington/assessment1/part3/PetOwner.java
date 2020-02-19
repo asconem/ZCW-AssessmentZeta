a03d7f2d57aab1ca33f6e0550b178528227dec4f
asconem <asconem@gmail.com>
2020-02-19 17:59:37 -0500

    Updating palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -10,20 +10,32 @@ public class PetOwner {
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
+    String name;
+    private ArrayList<Pet> pets = new ArrayList<>();
 
     public PetOwner(String name, Pet... pets) {
+        this.name = name;
+        if (pets != null){
+            for (Pet petElement : pets) {
+                this.pets.add(petElement);
+                petElement.setOwner(this);
+            }
+        }
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+        pets.add(pet);
+
     }
 
     /**
      * @param pet pet to be removed from the composite collection Pets
      */
     public void removePet(Pet pet) {
+        pets.remove(pet);
     }
 
     /**
@@ -31,14 +43,22 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
-        return null;
+
+        return (pets.contains(pet));
     }
 
     /**
      * @return the age of the Pet object whose age field is the lowest amongst all Pets in this class
      */
     public Integer getYoungetPetAge() {
-        return null;
+
+        Integer youngestPet = pets.get(0).getAge();
+        for (int i = 0; i < pets.size(); i++) {
+            if (pets.get(i).getAge() < youngestPet){
+                youngestPet = pets.get(i).getAge();
+            }
+        }
+        return youngestPet;
     }
 
 
@@ -48,7 +68,14 @@ public class PetOwner {
      * @return the age of the Pet object whose age field is the highest amongst all Pets in this class
      */
     public Integer getOldestPetAge() {
-        return null;
+
+        Integer oldestPet = pets.get(0).getAge();
+        for (int i = 0; i < pets.size(); i++) {
+            if (pets.get(i).getAge() > oldestPet){
+                oldestPet = pets.get(i).getAge();
+            }
+        }
+        return oldestPet;
     }
 
 
@@ -56,27 +83,45 @@ public class PetOwner {
      * @return the sum of ages of Pet objects stored in this class divided by the number of Pet object
      */
     public Float getAveragePetAge() {
-        return null;
+
+        int sum = 0;
+        for (int i = 0; i < pets.size(); i++) {
+            sum += pets.get(i).getAge();
+        }
+        Float average = Float.valueOf(sum / pets.size());
+        return average;
     }
 
     /**
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+
+        return pets.size();
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+
+        return name;
     }
 
     /**
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
-        return null;
+
+        if (pets.size() == 0){
+            Pet[] noPets = new Pet[1];
+            noPets[0] = null;
+            return noPets;
+        }
+        Pet[] output = new Pet[pets.size()];
+        for (int i = 0; i < output.length; i++) {
+            output[i] = pets.get(i);
+        }
+        return output;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,22 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public Integer countPalindromes(String input) {
+// Checks for single instances of letter
+        int counter = 0;
+
+        // Checks all substrings using StringBuilder
+        for (int i = 0; i <= input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                String forward = input.substring(i, j);
+                String backward = new StringBuilder(forward).reverse().toString();
+                if (forward.equals(backward)) {
+                    counter++;
+                }
+
+            }
+        }
+
+        return counter;
     }
 }
